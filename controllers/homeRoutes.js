69cf5106e30099708e63ffa50ddfd8a9f6ee003d
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-05-22 16:59:25 +0800

    Added index.js to /controllers/api - Added post request routes for /login and /logout in /controllers/api/userRoutes.js - Added get request for /signup in controllers/homeRoutes.js - Set up api routes in /controllers/index.js - Added login.js, logout.js and signup.js in /public/js - Created auth.js in /utils - Added dashboard.handlebars and signup.handlebars in /views - Added css styles

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 // router.get('/', async(req, res) => {
 //   try {
@@ -22,8 +22,6 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-console.log("postData");
-    
 
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
@@ -69,6 +67,37 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    console.log('i am here')
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
